09627fe5288833e8a01ed61d741c453f0b7ed72a
Lakshmi-Boora <booralakshmi18@gmail.com>
2026-01-14 09:54:37 +0530

    Update axios base URL to use environment variable

--- frontend/src/api/axios.ts
+++ frontend/src/api/axios.ts
@@ -1,8 +1,17 @@
+// import axios from "axios";
+
+// const api = axios.create({
+//   baseURL: "import.meta.env.VITE_API_URL",
+//   withCredentials: true
+// });
+
+// export default api;
+
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:2000/api/v1",
-  withCredentials: true
+  baseURL: import.meta.env.VITE_API_URL,
+  withCredentials: true,
 });
 
 export default api;
